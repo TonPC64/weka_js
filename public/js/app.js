b22233e1969ba6d54b2819aa949fbcc7cdddd0d2
kingkong64 <kingkong2103@gmail.com>
2016-04-13 01:57:30 +0700

    select materialize

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,35 @@
 /* global angular */
-angular.module('wekaApp', [])
+angular.module('wekaApp', ['ui.materialize'])
   .controller('wekaCtrl', function ($scope, $http) {
-    $scope.post = function () {
-      var data = "'x','s','y','t','a','f','c','b','k','e','c','s','s','w','w','p','w','o','p','n','n','g',?"
-      $http.post('/data', {data: data}).success((req, res) => {
-        $scope.data = req
-      })
-    }
+    $scope.cap_shape = [
+      {name: 'bell', value: 'b'},
+      {name: 'conical', value: 'c'},
+      {name: 'convex', value: 'x'},
+      {name: 'flat', value: 'f'},
+      {name: 'knobbed', value: 'k'},
+      {name: 'sunken', value: 's'}
+    ]
+    $scope.odor = [
+      {name: 'almond', value: 'a'},
+      {name: 'anise', value: 'l'},
+      {name: 'creosote', value: 'c'},
+      {name: 'fishy', value: 'y'},
+      {name: 'foul', value: 'f'},
+      {name: 'musty', value: 'm'},
+      {name: 'none', value: 'n'},
+      {name: 'pungent', value: 'p'},
+      {name: 'spicy', value: 's'}
+    ]
 
-    $scope.post2 = function () {
-      var data = "'x','s','n','t','p','f','c','n','k','e','e','s','s','w','w','p','w','o','p','k','s','u',?"
-      $http.post('/data', {data: data}).success((req, res) => {
-        $scope.data = req
+    $scope.form = ['x', 's', 'y', 't', 'a', 'f', 'c', 'b', 'k', 'e', 'c', 's', 's', 'w', 'w', 'p', 'w', 'o', 'p', 'n', 'n', 'g']
+    $scope.post = function () {
+      $scope.data = "'" + $scope.form.join("','") + "',?"
+      $http.post('/data', {data: $scope.data}).success((req, res) => {
+        $scope.result = req
       })
     }
   })
+
+$(document).ready(function () {
+  $('select').material_select()
+})
