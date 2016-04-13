0676a64d1a7e0425edf3728a724d70576f474201
kingkong64 <kingkong2103@gmail.com>
2016-04-14 02:24:55 +0700

    footter

--- public/js/app.js
+++ public/js/app.js
@@ -7,29 +7,45 @@ angular.module('wekaApp', ['ui.materialize'])
     })
     $scope.form = ['x', 's', 'y', 't', 'a', 'f', 'c', 'b', 'k', 'e', 'c', 's', 's', 'w', 'w', 'p', 'w', 'o', 'p', 'n', 'n', 'g']
     $scope.post = function () {
+      $scope.showModel = false
       $scope.data = "'" + $scope.form.join("','") + "',?"
       $http.post('/data', {data: $scope.data}).success((req, res) => {
-        $scope.result = req
+        var result = req[2].split(' ').filter((item) => item !== '')
+        answer(result)
       })
     }
 
-    // var tranfrom = function (str) {
-    //   str = str.split(',')
-    //   var emp = ''
-    //   var gg = str.forEach(item => {
-    //     var temp = item.split('=')
-    //     emp += '{' + ['"name":"' + temp[0] + '"', '"value":"' + temp[1] + '"'].join(',') + '},'
-    //   })
-    //
-    //   return emp
-    // }
+    var answer = function (res) {
+      $scope.resRe = res[3]
+      var imgres = res[2].split(':')
+      if (imgres[1] === 'e') {
+        $('h4#modalH').removeClass('red-text')
+        $('h4#modalH').addClass('green-text')
+        $scope.resImg = 'img/edible.jpg'
+      } else if (imgres[1] === 'p') {
+        $('h4#modalH').removeClass('green-text')
+        $('h4#modalH').addClass('red-text')
+        $scope.resImg = 'img/poison.jpg'
+      }
+      $scope.showModel = true
+    }
 
-  // % 9. Class Distribution:
-  // %     --    edible: 4208 (51.8%)
-  // %     -- poisonous: 3916 (48.2%)
-  // %     --     total: 8124 instances
+  // var tranfrom = function (str) {
+  //   str = str.split(',')
+  //   var emp = ''
+  //   var gg = str.forEach(item => {
+  //     var temp = item.split('=')
+  //     emp += '{' + ['"name":"' + temp[0] + '"', '"value":"' + temp[1] + '"'].join(',') + '},'
+  //   })
+  //
+  //   return emp
+  // }
   })
 
 $(document).ready(function () {
   $('select').material_select()
+})
+
+$(document).ready(function () {
+  $('.modal-trigger').leanModal()
 })
